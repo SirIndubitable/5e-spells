6b0bea7721a1afbb70d21f47c13fd4ea448e26d4
Matt O <sirindubitable@gmail.com>
2018-06-25 23:11:02 -0500

    Made a better way to parse files, it's now a ton easier to add single spells

--- source/parse-spells.py
+++ source/parse-spells.py
@@ -1,6 +1,7 @@
 import argparse
 import re
 import json
+import os
 import doctest
 
 
@@ -19,7 +20,7 @@ def parse_type(line):
     >>> parse_type("Conjuration cantrip")
     0, Conjuration
     >>> parse_type("2nd-level abjuration")
-    2, abjuration
+    2, Abjuration
     """
     words = line.split()
     # if the first word doesn't contain a number, it's probably a cantrip
@@ -29,47 +30,39 @@ def parse_type(line):
         return 0, words[0].capitalize()
 
 
-def parse_spell(spell):
+def add_spell(spellDict, spell):
     spell_iter = iter(spell)
-    next(spell_iter)  # Throw away the spell name
+    spell_name = next(spell_iter).strip()
     spell_info = {}
     spell_info["level"], spell_info["school"] = parse_type(next(spell_iter))
-    spell_info["casting_time"] = next(spell_iter).replace('Casting Time: ', '')
+    spell_info["casting_time"] = next(spell_iter).replace('Casting Time: ', '').strip()
     spell_info["range"] = next(spell_iter).replace('Range: ', '').strip()
-    spell_info["components"] = next(spell_iter).replace('Components: ', '')
-    spell_info["duration"] = next(spell_iter).replace('Duration: ', '')
-    spell_info["description"] = " ".join(spell_iter)
-    return spell_info
+    spell_info["components"] = next(spell_iter).replace('Components: ', '').strip()
+    spell_info["duration"] = next(spell_iter).replace('Duration: ', '').strip()
+    spell_info["description"] = " ".join(map(str.strip, spell_iter))
+    spellDict[spell_name] = spell_info
 
 
-def parse_file(spell_list, spell_descs):
-    spell_list_file = open(spell_list, 'r', encoding="utf8")
-    spells = iter(spell_list_file.readlines())
-    current_spell, next_spell = get_next_spell(next(spells), spells)
+def parse_files():
     spellDict = {}
-    lines = []
-    for spell_description_line in open(spell_descs, 'r', encoding="utf8"):
-        if spell_description_line == next_spell:
-            spellDict[current_spell.strip()] = parse_spell(lines)
-            lines = []
-            current_spell, next_spell = get_next_spell(next_spell, spells)
-        lines.append(spell_description_line.strip())
+    script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+    spells_dir = os.path.join(script_dir, 'spells')
+    for file in os.listdir(spells_dir):
+        filename = os.path.join(spells_dir, os.fsdecode(file))
+        spell = open(filename, 'r', encoding="utf8")
+        add_spell(spellDict, spell)
+        spell.close()
 
-    # add the last spell
-    spellDict[current_spell.strip()] = parse_spell(lines)
     return spellDict
 
 
 parser = argparse.ArgumentParser(description='Parses spells into json')
-parser.add_argument(dest='spell_list', type=str,
-                    help='flat list of all spells')
-parser.add_argument(dest='spell_desc', type=str,
-                    help='description of all the spells')
 parser.add_argument(dest='spell_dest', type=str,
                     help='destination of all the spells json file')
 
 args = parser.parse_args()
-spells_dict = parse_file(args.spell_list, args.spell_desc)
+
+spells_dict = parse_files()
 json.dumps(spells_dict)
 f = open(args.spell_dest, 'w')
 f.write(json.dumps(spells_dict, indent=3, sort_keys=True))
