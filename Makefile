14fc1c6079e9a509178284658b375cb6f04570b2
Matt O <sirindubitable@gmail.com>
2018-06-27 21:21:30 -0500

    Got class list working, need to add missing spells

--- Makefile
+++ Makefile
@@ -1,2 +1,2 @@
 spells.json: source/parse-spells.py $(wildcard source/spells/*.txt)
-	python source/parse-spells.py -json="spells.json" -list="spelllist.txt"
+	python source/parse-spells.py -json="spells.json" -list="spelllist.txt" -class="List of all 5e spells.txt"
--- source/parse-spells.py
+++ source/parse-spells.py
@@ -5,6 +5,9 @@ import os
 import doctest
 
 
+missing_spells = set()
+
+
 def get_next_spell(next_spell, spell_itr):
     current_spell = next_spell
     try:
@@ -30,21 +33,46 @@ def parse_type(line):
         return 0, words[0].capitalize()
 
 
+def parse_value(line, valueName):
+    """Parses the school and level out of the standard string
+
+    >>> parse_type("Conjuration cantrip")
+    0, Conjuration
+    >>> parse_type("2nd-level abjuration")
+    2, Abjuration
+    """
+    return line.replace(valueName + ': ', '').strip()
+
+
 def add_spell(spellDict, spell):
     spell_iter = iter(spell)
     spell_name = next(spell_iter).strip()
-    spell_info = {}
+    spell_info = dict()
     spell_info["level"], spell_info["school"] = parse_type(next(spell_iter))
-    spell_info["casting_time"] = next(spell_iter).replace('Casting Time: ', '').strip()
-    spell_info["range"] = next(spell_iter).replace('Range: ', '').strip()
-    spell_info["components"] = next(spell_iter).replace('Components: ', '').strip()
-    spell_info["duration"] = next(spell_iter).replace('Duration: ', '').strip()
+    spell_info["casting_time"] = parse_value(next(spell_iter), 'Casting Time')
+    spell_info["range"] = parse_value(next(spell_iter), 'Range')
+    spell_info["components"] = parse_value(next(spell_iter), 'Components')
+    spell_info["duration"] = parse_value(next(spell_iter), 'Duration')
     spell_info["description"] = "".join(spell_iter)
+    spell_info['classes'] = list()
     spellDict[spell_name] = spell_info
 
 
+def add_classes(spellDict):
+    script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
+    classes_dir = os.path.join(script_dir, 'spellListByClass')
+    for file in os.listdir(classes_dir):
+        filename = os.path.join(classes_dir, file)
+        for spell in open(filename, 'r', encoding="utf8"):
+            spellName = spell.strip()
+            if spellName in spellDict:
+                spellDict[spellName]['classes'].append(file.replace('.txt', ''))
+            else:
+                missing_spells.add(spellName)
+
+
 def parse_files():
-    spellDict = {}
+    spellDict = dict()
     script_dir = os.path.dirname(__file__)  # <-- absolute dir the script is in
     spells_dir = os.path.join(script_dir, 'spells')
     for file in os.listdir(spells_dir):
@@ -52,7 +80,6 @@ def parse_files():
         spell = open(filename, 'r', encoding="utf8")
         add_spell(spellDict, spell)
         spell.close()
-
     return spellDict
 
 
@@ -60,10 +87,17 @@ parser = argparse.ArgumentParser(description='Parses spells into json')
 parser.add_argument('-json', dest='json_dest', type=str,
                     help='destination of the spells json file')
 parser.add_argument('-list', dest='list_dest', type=str,
-                    help='destination of the flat list of spells file')
+                    help='destination of the list of spells file')
+parser.add_argument('-class', dest='class_dest', type=str,
+                    help='destination of the file with the list of spells and classes')
+
 
 args = parser.parse_args()
 spells_dict = parse_files()
+add_classes(spells_dict)
+#print("Missing spells: \n")
+#print('\n'.join(sorted(missing_spells)))
+
 
 if args.json_dest is not None:
     f = open(args.json_dest, 'w')
@@ -74,4 +108,16 @@ if args.list_dest is not None:
     f = open(args.list_dest, 'w')
     for spell_name in sorted(spells_dict.keys()):
         f.write(spell_name + '\n')
+    f.close()
+
+if args.class_dest is not None:
+    f = open(args.class_dest, 'w')
+    spells_by_level = {0: [], 1: [], 2: [], 3: [], 4: [],
+                       5: [], 6: [], 7: [], 8: [], 9: []}
+    for spell_name, spell in spells_dict.items():
+        spells_by_level[spell['level']].append(spell_name)
+    for spell_level in sorted(spells_by_level.keys()):
+        f.write(str(spell_level) + '\n')
+        for spell in spells_by_level[spell_level]:
+            f.write(spell + ' (' + ', '.join(spells_dict[spell]['classes']) + ')\n')
     f.close()
